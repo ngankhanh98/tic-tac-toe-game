1dec39e121b6261f0735e4d391be0eb7c18ace13
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2019-09-23 14:01:47 +0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,136 +1,187 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'antd/lib';
-import { FaTimes } from "react-icons/fa";
-import {
-  FaRegCircle
 
-} from "react-icons/fa";
-
-
-class Square extends React.Component {
-  render() {
-    const value = this.props.value;
-    var icon = null;
-    if (value) {
-      icon = value === "X" ? <FaTimes className="cross" /> : <FaRegCircle className="nought" />;
-    }
-    return (
-      <Button className="square" type="default" onClick={() => this.props.onClick()}>
-        {icon}
-      </Button>
-    );
-  };
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(400).fill(null),
-      xNext: true,
-      winner: null,
-    };
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
-
   createBoard() {
     let board = []
     for (let i = 0; i < 20; i++) {
       let row = []
       for (let j = 0; j < 20; j++) {
-        row.push(<Square value={this.state.squares[20 * i + j]} onClick={() => this.handleClick(20 * i + j)} />)
+        /*row.push(<Square value={this.state.squares[20 * i + j]} onClick={() => this.handleClick(20 * i + j)} />)*/
+        row.push(this.renderSquare(20*i+j))
       }
       board.push(<div className="board-row">{row}</div>);
     }
     return board;
   }
+  render() {
+    return (
+      this.createBoard()
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(400).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+      winner: null
+    };
+  }
 
   handleClick(i) {
-    const squares = this.state.squares.slice(); // create a copy of this.state.squares
-    if (squares[i] !== null || this.state.winner !== null)
-      return;
-    squares[i] = this.state.xNext ? 'X' : 'O';
-    this.setState({ squares: squares, xNext: !this.state.xNext }); // pass the replicant squares array into the original squares array
+    const history = this.state.history.slice(0, this.state.stepNumber + 1); 
+    const board = this.state.history[this.state.stepNumber];
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
 
-    let row = Math.floor(i / 20);
+    if(this.state.winner || squares[i])
+    { 
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    
+    this.setState({ winner: calculateWinner(i, board, squares[i])});
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = this.state.winner;
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={i => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+function calculateWinner(i, squares, value) {
+  let row = Math.floor(i / 20);
     let col = i % 20;
     let count = 0;
+    let winner = null;
+    
+    console.log(squares.squares[1]);
+    console.log(row, col);
 
     // check row
     for (let k = 0; k < 20; k++) {
-      if (squares[row * 20 + k] !== squares[i]) {
+      if (squares.squares[row * 20 + k] !== value) {
         count = 0;
+        
       }
       else count++;
-
-      if (count === 5) {
-        this.setState({ winner: squares[i] });
-        console.log("Winner " + squares[i]);
+      if (count === 4) {
+        return value;
       }
+      
     }
 
     // check col
     for (let k = 0; k < 20; k++) {
-      if (squares[k * 20 + col] !== squares[i]) {
+      if (squares.squares[k * 20 + col] !== value) {
         count = 0;
       }
       else count++;
-      if (count === 5) {
-        this.setState({ winner: squares[i] });
-        console.log("Winner " + squares[i]);
+      if (count === 4) {
+        return value;
       }
     }
     // check diagonal
     let inital_pos = i % 21;
     for (let k = 0; k < 20; k++) {
-      if (squares[inital_pos + 21 * k] !== squares[i]) {
+      if (squares.squares[inital_pos + 21 * k] !== value) {
         count = 0;
       }
       else count++;
-      if (count === 5) {
-        this.setState({ winner: squares[i] });
-        console.log("Winner " + squares[i]);
+      if (count === 4) {
+        return value;
       }
     }
     // check anti-diagonal
     let inital_pos_anti = i % 21 + 2 * row;
-    console.log(i);
-    console.log(inital_pos_anti);
+   // console.log(i);
+    //console.log(inital_pos_anti);
     for (let k = 0; k < 20; k++) {
-      if (squares[inital_pos_anti + 19 * k] !== squares[i]) {
+      if (squares.squares[inital_pos_anti + 19 * k] !== value) {
         count = 0;
       }
       else count++;
-      if (count === 5) {
-        this.setState({ winner: squares[i] });
-        console.log("Winner " + squares[i]);
+      if (count === 4) {
+        return value;
       }
     }
-  }
-  handleClickReset() {
-    const original = Array(400).fill(null);
-    this.setState({ squares: original, winner: null, xNext: true });
-  }
-  render() {
-    let status;
-    if (this.state.winner) {
-      status = 'Winner: ' + this.state.winner;
-    } else {
-      status = 'Next player: ' + (this.state.xNext ? 'X' : 'O');
-    }
-    return (
-      <div className="container">
-        <div className="status">{status}</div>
-        <div className="game">
-          <div className="game-board">
-            {this.createBoard()}
-          </div>
-        </div>
-        <div className="button-reset"><Button type="primary" background-color="#0da192" onClick={() => this.handleClickReset()}>RESET THE GAME</Button></div>
-      </div>
 
-    )
-  }
+   
+    return winner;
 }
 
-export default Board;
+export default Game;
